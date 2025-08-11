74b99a4c0303679500a561c79c25ccf94ffae3e7
my935 <my935@LAPTOP-O5N3Q2TF.mshome.net>
2025-08-11 16:22:18 +0530

    upadte in java utility

--- crmFramework/src/main/java/com/comcast/crm/generic/javaUtility/javacUtility.java
+++ crmFramework/src/main/java/com/comcast/crm/generic/javaUtility/javacUtility.java
@@ -1,5 +1,17 @@
 package com.comcast.crm.generic.javaUtility;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class javacUtility {
+	
+	
+	public String getSystemDate() {
+		
+		Date dateobj=new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String date = sdf.format(dateobj);
+		return date;
+	}
 
 }
